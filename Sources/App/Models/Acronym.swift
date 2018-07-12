c1e44ac6aae0f9010e6b86d4a59e1f4f278aa171
Kiren Srinivasan <srinitude@gmail.com>
2018-07-12 09:49:25 -0700

    first commit

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,17 @@
+import FluentSQLite
+import Vapor
+
+final class Acronym: Codable {
+    var id: Int?
+    var short: String
+    var long: String
+    
+    init(short: String, long: String) {
+        self.short = short
+        self.long = long
+    }
+}
+
+extension Acronym: Content { }
+extension Acronym: Migration { }
+extension Acronym: SQLiteModel { }
